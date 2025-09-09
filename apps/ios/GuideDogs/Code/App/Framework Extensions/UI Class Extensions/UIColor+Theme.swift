8ca4ec22d209223b4d68d99f7eecfb9ea28a6940
Gonzalo <quaringonzalo@gmail.com>
2025-09-08 22:59:11 -0300

    Agregar nuevos esquemas de colores en la carpeta deprecated-colors y un nuevo color de fondo secundario. Se han creado múltiples archivos de configuración de colores en formato JSON para varios tonos, incluyendo darkBlue, darkGray, darkTeal, darkYellow, extraDarkGray, extraLightGray, gray, lightBlue, lightGray, lightTeal y lightYellow.

--- apps/ios/GuideDogs/Code/App/Framework Extensions/UI Class Extensions/UIColor+Theme.swift
+++ apps/ios/GuideDogs/Code/App/Framework Extensions/UI Class Extensions/UIColor+Theme.swift
@@ -12,29 +12,48 @@ extension UIColor {
     
     enum Theme {
         
-        // MARK: Colors
+        // MARK: Main
+        static let yellow = UIColor(hex: "#FFBA00")
+        static let teal = UIColor(hex: "#008575")
+        static let blue = UIColor(hex: "#0078D4")
         
-        static let yellow = UIColor(red: 1.0, green: 0.73, blue: 0.0, alpha: 1.0)
-        static let teal = UIColor(red: 0.0, green: 0.52, blue: 0.46, alpha: 1.0)
-        static let blue = UIColor(red: 0.0, green: 0.47, blue: 0.83, alpha: 1.0)
         // Dark
-        static let darkYellow = UIColor(red: 0.42, green: 0.29, blue: 0.09, alpha: 1.0)
-        static let darkTeal = UIColor(red: 0.15, green: 0.29, blue: 0.28, alpha: 1.0)
-        static let darkBlue = UIColor(red: 0.14, green: 0.23, blue: 0.37, alpha: 1.0)
+        static let darkYellow = UIColor(hex: "#6B4A17")
+        static let darkTeal = UIColor(hex: "#264A47")
+        static let darkBlue = UIColor(hex: "#164792")
+        
         // Light
-        static let lightYellow = UIColor(red: 1.0, green: 0.94, blue: 0.0, alpha: 1.0)
-        static let lightTeal = UIColor(red: 0.19, green: 0.90, blue: 0.82, alpha: 1.0)
-        static let lightBlue = UIColor(red: 0.31, green: 0.90, blue: 1.0, alpha: 1.0)
+        static let lightYellow = UIColor(hex: "#FFF000")
+        static let lightTeal = UIColor(hex: "#30E6D1")
+        static let lightBlue = UIColor(hex: "#4FE6FF")
         
-        // MARK: Neturals
-        
-        static let extraLightGray = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.0)
-        static let lightGray = UIColor(red: 0.90, green: 0.90, blue: 0.90, alpha: 1.0)
-        static let gray = UIColor(red: 0.82, green: 0.82, blue: 0.82, alpha: 1.0)
-        static let midGray = UIColor(red: 0.45, green: 0.45, blue: 0.45, alpha: 1.0)
-        static let darkGray = UIColor(red: 0.31, green: 0.31, blue: 0.31, alpha: 1.0)
-        static let extraDarkGray = UIColor(red: 0.18, green: 0.18, blue: 0.18, alpha: 1.0)
+        // MARK: Neutrals
+        static let extraLightGray = UIColor(hex: "#F2F2F2")
+        static let lightGray = UIColor(hex: "#E5E5E5")
+        static let gray = UIColor(hex: "#D1D1D1")
+        static let midGray = UIColor(hex: "#737373")
+        static let darkGray = UIColor(hex: "#4F4F4F")
+        static let extraDarkGray = UIColor(hex: "#2E2E2E")
         
     }
-    
+}
+
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if hexSanitized.hasPrefix("#") {
+            hexSanitized.remove(at: hexSanitized.startIndex)
+        }
+        
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+        
+        let r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let b = CGFloat(rgb & 0x0000FF) / 255.0
+        
+        self.init(red: r, green: g, blue: b, alpha: alpha)
+    }
 }
